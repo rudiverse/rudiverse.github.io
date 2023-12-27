44cc53e9ff14eac02a3d69d8224261d347b33c53
mumemo <rudimoritz@gmail.com>
2023-12-27 18:17:13 +1300

    Evaluate quiz, and next question

--- _quizzes/quiz_001.md
+++ _quizzes/quiz_001.md
@@ -4,8 +4,10 @@ shuffleQuestions: false
 shuffleAnswers: true
 ---
 <quiz>
-    <button id="loadQuizButton" onclick="buildQuiz('loadQuizButton')">Load quiz</button>
+    <button id="loadQuizButton" onclick="startQuiz('loadQuizButton')" class="quiz_button">Load quiz</button>
     <h3 id="quiz_question"></h3>
     <div id="quiz_answers"></div>
+    <div id="quiz_submit_button"></div>
     <div id="quiz_evaluation"></div>
+    <div id="quiz_next_button"></div>
 </quiz>
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,18 +3,33 @@
 
 let answerSelected = ""
 
-/// buildQuiz
-function buildQuiz(btnToHideId) {
-    const btnToHide = document.getElementById(btnToHideId)
-    btnToHide.style.display = "none";
+function startQuiz(btnToHideId) {
+    if (btnToHideId != "") {
+        const btnToHide = document.getElementById(btnToHideId)
+        btnToHide.style.display = "none";
+    }
+
+    buildQuiz(-1)
+}
+
+function buildQuiz(skipQuestionId) {
+    const nextButtonElement = document.getElementById("quiz_next_button")
+    nextButtonElement.replaceChildren()
+    
+    const evaluationElement = document.getElementById("quiz_evaluation")
+    evaluationElement.replaceChildren()
 
     let quizItemIndex = Math.floor(Math.random() * quizData.length)
+    while (quizItemIndex == skipQuestionId) {
+        quizItemIndex = Math.floor(Math.random() * quizData.length)
+    }
     let quizItem = quizData[quizItemIndex]
 
     const questionElement = document.getElementById("quiz_question")
     questionElement.innerHTML = quizItem.question
 
     const answerElement = document.getElementById("quiz_answers")
+    answerElement.replaceChildren()
 
     quizItem.answers.forEach((item, index) => {
         var answerId = "answer_" + index
@@ -42,12 +57,16 @@ function buildQuiz(btnToHideId) {
 
     var submitButton = document.createElement("button")
     submitButton.setAttribute('onclick', "evaluateQuestion(" + quizItemIndex + ")")
-    submitButton.innerHTML = "Go"
-    answerElement.appendChild(submitButton)
+    submitButton.innerHTML = "GO"
+    submitButton.className = "quiz_button"
+
+    const submitButtonElement = document.getElementById("quiz_submit_button")
+    submitButtonElement.appendChild(submitButton)
 }
 
 function evaluateQuestion(quizItemIndex) {
-    alert("You clicked on answer " + answerSelected + " of question " + (quizItemIndex + 1))
+    const submitButtonElement = document.getElementById("quiz_submit_button")
+    submitButtonElement.replaceChildren()
 
     let quizItem = quizData[quizItemIndex]
     let answerFound = quizItem.answers.find(x => x.answer == answerSelected)
@@ -56,10 +75,17 @@ function evaluateQuestion(quizItemIndex) {
 
     if (answerFound.valid) {
         evaluationElement.innerHTML = "Well done, you nailed it!"
+        evaluationElement.className = "quiz_evaluation_correct"
     } else {
         evaluationElement.innerHTML = "Oh no, you need to study more."
+        evaluationElement.className = "quiz_evaluation_wrong"
     }
 
+    var nextButton = document.createElement("button")
+    nextButton.setAttribute('onclick', "buildQuiz(" + quizItemIndex + ")")
+    nextButton.innerHTML = "NEXT"
+    nextButton.className = "quiz_button"
 
-
+    const nextButtonElement = document.getElementById("quiz_next_button")
+    nextButtonElement.appendChild(nextButton)
 }
